93f01d7199dcf6a07e5b8dbd025f05ca05296b2a
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2017-11-17 13:40:18 -0600

    I tried

---
+++ Answers.md
@@ -0,0 +1,22 @@
+## Questions
+## What are the order of insertions/removals for the following data structures? 
+* Stack - First in, Last out
+* Queue - First in, first out
+## What is the retreival time complexity for the following data structures? 
+* Linked List 
+low complexity to implement
+inserts are O(1)
+lookups are O(N)
+* Hash Table
+high complexity to implement
+inserts are O(1) on average
+lookups are O(1) on average
+* Binary Search Trees 
+medium complexity to implement (assuming you can't get them from a library)
+inserts are O(logN)
+lookups are O(logN)
+## What are some advantages to using a Hash Tables over an array in JavaScript?
+* Speed.
+* Efficiency.
+* Reduces average lookup costs.
+* No collisions.
--- src/hash-table.js
+++ src/hash-table.js
@@ -70,3 +70,11 @@ class HashTable {
 }
 
 module.exports = HashTable;
+
+// I googled for a straight hour if not longer, 
+// I also looked at my linked list from last week and it didn't give me a clear picture either, 
+// I barely get a grasp on what we are learning then have to relearn it the next week causing a backup in my brain.
+// so this was not completed but not for a lack of effort but for being completely confused about what you want from me.
+// I find these impossible to read in the first place and now the instructions are even more wonky with this one. 
+// I am doing what is asked of me and instead of coping and pasting I am taking the time to tell you i am lost. 
+// so I am going to spend the next twenty min plus lunch to watch videos and more videos and read on this again.
